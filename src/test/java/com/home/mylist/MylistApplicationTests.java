e1164619f6172360df68f8ff388b9585f53e7038
home <viostarryy@gmail.com>
2024-01-26 14:58:10 +0900

    initial commit

---
+++ src/test/java/com/home/mylist/MylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.mylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
